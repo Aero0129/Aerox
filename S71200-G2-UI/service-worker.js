b6d1b0f605c17243b67d425044e29b45931c282c
Aero0129 <62086165+Aero0129@users.noreply.github.com>
2026-09-13 15:39:29 +0800

    Add files via upload

--- S71200-G2-UI/service-worker.js
+++ S71200-G2-UI/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "s7-1200-g2-finder-shell-v42";
+const CACHE_NAME = "s7-1200-g2-finder-shell-v44";
 const CACHE_PREFIX = "s7-1200-g2-finder-shell-";
 
 const ROOT_URL = new URL("./", self.location.href).href;
